94a0c317544e34883a97b396be580d21f3bdfead
“dkangbrown” <dongyoon_kang@brown.edu>
2025-05-06 22:15:52 -0400

    Adding support repo files

--- query_fireworks.py
+++ query_fireworks.py
@@ -1,22 +1,24 @@
 import requests
 import json
 
-url = "https://api.fireworks.ai/inference/v1/chat/completions"
+url = "https://api.fireworks.ai/inference/v1/completions"
 payload = {
   "model": "accounts/dongyoon-kang-a70308/deployedModels/ft-avart-garde-v2-prompt-yytv2pwt",
-  "max_tokens": 200,
-#   "logprobs": 3,
+  "max_tokens": 500,
+  "logprobs": 3,
   "top_p": 1,
   "top_k": 40,
   "presence_penalty": 0,
   "frequency_penalty": 0,
-  "temperature": 0.6,
-  "messages": [
-    {
-      "role": "user",
-      "content": "Write me an Avant-Garde manifesto."
-    }
-  ]
+  "temperature": 0.5,
+  "prompt": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\nYou are an Avant-Garde artist, one of the Cubists, Futurists, Orphists, Dadaists, or Surrealists who believe modernity has changed what it means to be an artist\
+  and what it means to be human. You create art that is innovative, experimental, and pushes the boundaries of established artistic norms.<|eot_id|><|start_header_id|>user<|end_header_id|>\nWrite me an Avant-Garde manifesto.<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n",
+  # "messages": [
+  #   {
+  #     "role": "user",
+  #     "content": "Write me an Avant-Garde manifesto."
+  #   }
+  # ] 
 }
 headers = {
   "Accept": "application/json",
